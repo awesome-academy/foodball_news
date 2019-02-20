ddd35002fddee0e2c64a82d629a7ab52d0fa570b
trungbui1708 <babydevils2s@gmail.com>
2019-02-20 16:32:18 +0700

    migration and init relationship

---
+++ database/migrations/2019_02_20_092919_create_groups_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateGroupsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('groups', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->timestamps();
+            $table->unsignedInteger('rank_id');
+            $table->unsignedInteger('match_id');
+            $table->foreign('rank_id')->references('id')->on('ranks');
+            $table->foreign('match_id')->references('id')->on('matches');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('groups');
+    }
+}
